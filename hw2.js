20a079b00e9317d862e94503fcfbce6faf43b1f2
Xenia <xeniaklim2000@yandex.ru>
2021-11-06 23:36:03 +0300

    homework 2

---
+++ hw2.js
@@ -0,0 +1,21 @@
+console.log(5>4); //true
+console.log('ананас'>'яблоко'); //false
+console.log('2'>'12'); //true
+console.log(undefined==null);//true
+console.log(undefined===null); //false
+console.log(null=='\n0\n'); //false
+console.log(null===+'\n0\n'); //false
+console.log(''+1+0); //'10'
+console.log(''-1+0); //-1
+console.log(true+false); //1
+console.log(6/'3'); //2
+console.log('2'*'3'); //6
+console.log(4+5+'px'); //'9px'
+console.log('$'+(4+5)); //'$9'
+console.log('4'-2); //2
+console.log(7/0); //infinity
+console.log('-9'+5); //'-95'
+console.log('-9'-5); //-14
+console.log(null+1); //1
+console.log(undefined+1); //NaN
+console.log('\t\n'-2); //-2
